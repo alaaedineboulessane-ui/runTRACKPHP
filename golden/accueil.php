0d46fc07254b04530a7cb855f63f8be84635a229
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2026-01-27 09:23:27 +0100

    ajout de css sur les boutons

--- golden/accueil.php
+++ golden/accueil.php
@@ -1,5 +1,5 @@
 <?php include './includes/header.php'; ?>
-
+<h1>Ceci est un site qui permet de laisser votre avis sur les mangas les plus celèbres !</h1>
 
 <?php include './includes/footer.php'; ?>
 </body>
