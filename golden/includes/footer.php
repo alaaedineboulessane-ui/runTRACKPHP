8fb3e0ec0388d0f8dea534b4975b316bdd0a4519
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2026-01-30 08:49:14 +0100

    styilisation de la page edit

--- golden/includes/footer.php
+++ golden/includes/footer.php
@@ -2,7 +2,7 @@
 
 <link href = "../css/footer.css" rel = "stylesheet">
 <a href = "../php/avis.php">
-    <h1 id = "avis">Page d'avis</h1>
+    <h1 id = "aviss">Page d'avis</h1>
 </a>
 </footer>
 </div></div>
