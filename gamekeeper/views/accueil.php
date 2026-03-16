2fab45d2baed13898512d5cfa30edb0b286dd6b0
alaaedineboulessane-ui <alaaedine.boulessane@laplateforme.io>
2026-03-16 10:41:56 +0100

    debut du projet

--- gamekeeper/views/accueil.php
+++ gamekeeper/views/accueil.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Accueil</title>
-    <link rel="stylesheet" href="../style/css/accueil.css">
+    <link rel="stylesheet" href="./assets/css/accueil.css">
 </head>
 <body>
     <h1>Bienvenue sur votre collection de jeux !</h1>
-    <p>Ceci est la page d’accueil.</p>
+    <p>Ici vous pourrez ajouter des jeux à votre collection.</p>
 </body>
 </html>
